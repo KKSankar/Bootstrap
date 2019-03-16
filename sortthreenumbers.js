36c2c174937b55e49ff7e29f21ae6897b1b7e037
Karthika Kumar <karthikakumar91@gmail.com>
2019-03-16 15:56:20 -0700

    first commit

---
+++ sortthreenumbers.js
@@ -0,0 +1,12 @@
+var x = parseInt(prompt("Enter a number"));
+var y = parseInt(prompt("Enter a number"));
+var z = parseInt(prompt("Enter a number"));
+if ((x > y) && (x > z)) {
+alert ("x is greater");
+}
+else if ((y>x) && (y>z)) {
+    alert ("y is greater");
+}
+else if ((z>x) && (z>y)) {
+    alert ("z is greater");
+}
